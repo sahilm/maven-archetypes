ada82a572db6b05947a54307343469dd46f20397
Sahil Muthoo <sahil.muthoo@gmail.com>
2012-02-08 00:06:52 +0530

    Initial commit

---
+++ template/src/main/resources/archetype-resources/src/main/java/Foo.java
@@ -0,0 +1,8 @@
+package ${package};
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("Blank slate!");
+	}
+
+}
---
+++ template/src/main/resources/archetype-resources/src/test/java/FooTest.java
@@ -0,0 +1,8 @@
+package ${package};
+public class FooTest {
+	
+	@Test
+	public void niceTest() {
+		fail("Add tests here!");
+	}
+}
